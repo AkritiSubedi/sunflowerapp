d93ae37f3bb65730d2aaeb7c122f7375252866f6
AkritiSubedi <aakusubedi075@gmail.com>
2023-07-02 18:01:50 +0545

    flutter

---
+++ sunflowerapp/lib/main.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+import 'mysplashscreen.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MySplashScreen(),
+    ));
